44d5090a54e632597b175c8ffa02312bb75b5e82
oskarth <me@oskarth.com>
2015-05-14 20:06:09 +0200

    works

--- src/pmarca_chen/core.clj
+++ src/pmarca_chen/core.clj
@@ -1,6 +1,50 @@
-(ns pmarca-chen.core)
+(ns pmarca-chen.core
+  (:require [clojure.set :as set]
+            [twitter.oauth :as oauth]
+            [twitter.api.restful :as api]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(def old-tweets (atom #{}))
+
+(def my-creds (oauth/make-oauth-creds
+               (System/getenv "PMARCACHEN_CONSUMER_KEY")
+               (System/getenv "PMARCACHEN_CONSUMER_SEC")
+               (System/getenv "PMARCACHEN_ACCESS_TOKEN")
+               (System/getenv "PMARCACHEN_ACCESS_TOKEN_SEC")))
+
+(defn timeline []
+  (api/statuses-home-timeline :oauth-creds my-creds))
+
+(defn fetch-tweets []
+  (set (map :id (:body (timeline)))))
+
+(defn retweet! [tweet]
+  (do (api/statuses-retweet-id :oauth-creds my-creds
+                               :params {:id tweet})
+      (prn "Tweeted " tweet)))
+
+(defn maybe-retweet!
+  "Retweet a tweet 10% of the time."
+  [tweet]
+  (if (= (rand-int 10) 9)
+    (retweet! tweet)
+    (prn (str "Discarded tweet " tweet))))
+
+(defn fetch-and-retweet!
+  "Fetches tweets, retweets some, and calculates new tweets, and maybe
+  retweets them. Adds new tweets to old tweets set."
+  []
+  (let [all-tweets (fetch-tweets)
+        new-tweets (set/difference all-tweets @old-tweets)]
+    (do
+      (println (str "Found " (count new-tweets) " new tweets."))
+      (dorun (map maybe-retweet! new-tweets))
+      (swap! old-tweets set/union new-tweets))))
+
+(defn periodically! [f ms]
+  (future (while true (do (Thread/sleep ms) (f)))))
+
+;; NOTE: Start polling at startup
+(def pmarca-chen (periodically! fetch-and-retweet! (* 1000 60 5)))
+
+;; Evaluate this to cancel pmarca-chen
+;; (future-cancel pmarca-chen)
